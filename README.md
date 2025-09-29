66766ad2cceb0c3f80e02dc9819bd5aec2d57907
Andreameza16 <yosoyandreabv@gmail.com>
2025-09-29 09:27:23 +0000

    cambios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 hola
+los comentariados # son lineas para conectarse a la base de datos en mysql, si se quiere la conexion quitarle el comentariado #, 
+se puede ejecutar normal sin requerir la conexion a la bd
--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'secret1342'
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/HOTEL_EL_MALINCHE'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/HOTEL_EL_MALINCHE'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
 # Inicializar SQLAlchemy
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
 # Ruta principal
 @app.route('/')
